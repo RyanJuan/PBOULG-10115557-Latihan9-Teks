b61e4a0e5116f19fc27926a66527062568bb2296
Rayan <37322518+RyanJuan@users.noreply.github.com>
2019-09-25 20:10:39 +0700

    Add files via upload

---
+++ PBOULG-10115557-Latihan9-Teks/src/pboulg/pkg10115557/latihan9/teks/PBOULG10115557Latihan9Teks.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboulg.pkg10115557.latihan9.teks;
+
+/**
+ *
+ * @author 10115557 / rayan / PBO Ulg / pertemuan 1 / latihan 9
+ */
+public class PBOULG10115557Latihan9Teks {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+	char c;
+	char tab = '\t';
+	char newline = '\n';
+	c = 'A';
+	System.out.println("Hasil" + newline + "Char: " + tab + "berisi " + c);
+	}
+}
+
